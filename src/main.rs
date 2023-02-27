50325b6dd7e3b5648fff6617d2ba985aad1d8f65
flooijdt <flooijdt@posteo.de>
2023-02-27 16:35:12 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,8 @@ fn recover_secret(triplets: Vec<[char; 3]>) -> String {
     // println!("{:?}", &word_vec);
     for i in triplets[1..].iter() {
         println!("{:?}", i);
-        for c in i.to_vec() {
-            if word_vec.contains(&c) {
+        for c in i.to_vec().iter() {
+            if i.position(|&c| c == 0) {
                 word_vec.push(c);
             }
         }
