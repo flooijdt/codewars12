f95f8b359ee06b6c70d5e31d263ca5460b3bf4b3
flooijdt <flooijdt@posteo.de>
2023-02-27 16:40:54 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,24 @@ fn main() {
 fn recover_secret(triplets: Vec<[char; 3]>) -> String {
     let mut word_vec: Vec<char> = triplets[0].into();
     // println!("{:?}", &word_vec);
-    for i in triplets[1..].iter() {
-        println!("{:?}", i);
-        for c in i.to_vec().iter() {
-            if i.position(|&c| c == 0) {
-                word_vec.push(c);
+    let mut lett: Vec<char> = Vec::new();
+
+    for i in triplets {
+        for c in i {
+            if !lett.contains(&c) {
+                lett.push(c);
             }
         }
     }
+
+    println!("{:?}", lett);
+    // for i in triplets[1..].iter() {
+    //     println!("{:?}", i);
+    //     for c in i.to_vec().iter() {
+    //         if i.position(|&c| c == 0) {
+    //             word_vec.push(c);
+    //         }
+    //     }
+    // }
     "String".to_string()
 }
