9404125aa613d35decda36158ea6ca52e017961f
flooijdt <flooijdt@posteo.de>
2023-02-27 15:01:28 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{:?}",
+        recover_secret(vec![
+            ['t', 'u', 'p'],
+            ['w', 'h', 'i'],
+            ['t', 's', 'u'],
+            ['a', 't', 's'],
+            ['h', 'a', 'p'],
+            ['t', 'i', 's'],
+            ['w', 'h', 's']
+        ])
+    );
+}
+
+fn recover_secret(triplets: Vec<[char; 3]>) -> String {
+    let mut word_vec: Vec<char> = triplets[0].into();
+    println!("{:?}", &word_vec);
+    for i in triplets {
+        for c in i {
+            if word_vec.is_empty() {
+                word_vec.push(c);
+            }
+        }
+    }
+    "String".to_string()
 }
